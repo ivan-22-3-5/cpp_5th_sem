40b9afdcfd30450461d4dd9c2a9f4958011849f6
Ivan Mihun <vsk76090@gmail.com>
2024-10-09 12:27:17 +0200

    fix join func

--- task2/src/stringtools.cpp
+++ task2/src/stringtools.cpp
@@ -16,7 +16,10 @@ std::vector<std::string> split_string(const std::string &str, const char delimit
 std::string join(const std::vector<std::string>& elements, const std::string& delimiter) {
     std::ostringstream result;
     for (size_t i = 0; i < elements.size(); ++i) {
-        result << (i != elements.size() - 1 ? elements[i] + delimiter[i] + " " : elements[i]);
+        result << elements[i];
+        if (i != elements.size() - 1) {
+            result << delimiter + " ";
+        }
     }
     return result.str();
 }
