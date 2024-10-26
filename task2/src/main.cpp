f618ecb77e0f7aceea336f2d460ab6ca60f09650
Ivan Mihun <vsk76090@gmail.com>
2024-10-26 19:17:41 +0200

    refactor(task2): rename parsing function

--- task2/src/main.cpp
+++ task2/src/main.cpp
@@ -6,12 +6,12 @@
 
 #include "stringtools.h"
 
-std::map<std::string, std::vector<std::string> > parse_args(int argc, char *argv[]);
+std::map<std::string, std::vector<std::string> > parse_filenames(char *filenames[], int n);
 
 int main(const int argc, char *argv[]) {
     std::vector<std::string> total;
     std::cout << "Files: " << std::endl;
-    for (const auto &[extension, filenames]: parse_args(argc, argv)) {
+    for (const auto &[extension, filenames]: parse_filenames(argv, argc)) {
         total.push_back(
             std::format("{} {} ", filenames.size(), extension) + (filenames.size() == 1 ? "file" : "files"));
         std::cout << "    " << extension << ": " << join(filenames, ",") << std::endl;
@@ -20,10 +20,10 @@ int main(const int argc, char *argv[]) {
     return EXIT_SUCCESS;
 }
 
-std::map<std::string, std::vector<std::string> > parse_args(const int argc, char *argv[]) {
+std::map<std::string, std::vector<std::string> > parse_filenames(char *filenames[], const int n) {
     std::map<std::string, std::vector<std::string> > files;
-    for (int i = 1; i < argc; ++i) {
-        std::string arg = argv[i];
+    for (int i = 1; i < n; ++i) {
+        std::string arg = filenames[i];
         if (auto tokens = split_string(arg, '.'); tokens.size() != 2) {
             throw std::runtime_error("Wrong format of filename!");
         } else {
